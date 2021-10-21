8ea94e9b0bd135ab2d5f393def1df02666eebf2e
debica8 <67833174+debica8@users.noreply.github.com>
2021-10-21 15:48:33 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<html lang="hr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Prva zadaća</title>
+        <link rel="stylesheet" href="stil.css">
+    </head>
+    <body>
+        <header>
+            <img src="slike/banner.jpg" />
+            <nav>
+                <ul>
+                  <li><a href="index.php">Početna stranica</a></li>
+                  <li><a href="vijesti.php">Vijesti</a></li>
+                  <li><a href="#">Kontakt</a></li>
+                  <li><a href="#">O nama</a></li>
+                  <li><a href="#">Galerija</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="sadrzaj">
+            <h1>Tattoo studio Tetobitna</h1>
+             <figure>
+                <img src="slike/karta.png" style="width: 50%;" >
+                <figcaption>Naše lokacije diljem RH</figcaption>
+            </figure>
+            <p>
+                Ideja o Tetobitna studiju nastala je 2015. godine kada je i otvoren prvi Tetobitna studio u Hrvatskoj.
+                Bilo je to u Otočcu. Danas uz salone u Splitu, Sinju, Rijeci, Osijeku, Zagrebu, Vodicama i Hvaru (otvoreni samo ljeti)
+                biti će uskoro otvoren salon i Bjelovaru.
+            </p>
+            <p>
+                Tetobitna postaje prvi Hrvatski lanac salona za tetoviranje, a svojom kvalitetom i higijenskim uvjetima te vrhunskim
+                majstorima spada u vodeće salone u zemji. Povremeno u salonima nudimo i usluge Body Piercing-a, a obavljaju ga vrhunski 
+                majstori pirsinga. Kroz studio Tetobitna danas, uz “majstorice” Lauru, Maci (Matea) i Debi prodefilirali su kao gosti:
+                Marko Ganić (Temple of Ink), Xeno (I.n.k. tattoo), Hrvoje (Hard Line), Damir (Hard Line), Lux i Ana (Oktania Tattoo),
+                Luka (Temple of Ink), Mario (Memories Tattoo). Tradicija gostovanja najboljih hrvatskih i stranih artista biti će nam 
+                i dalje visoko na listi prioriteta.
+            </p>
+            <p>
+                U studiju nudimo tetovaže: Tribal, Black & Grey, Old School, New School, Biomechanical, Letters (slova/natpisi), 
+                Tradition, portreti, realizam, orijentalne, Cartoon itd. Naše umjetnice će se prilagoditi vama, često na licu mjesta.
+                Slijedeći vašu želju u realnim okvirima. Gotovo svaki dizajn koji možete zamisliti, može se pretvoriti u lijepu tetovažu.
+                Dođite sa svojim idejama, predlošcima, fotografijama itd… kako biste zajedno sa nama stvorili vlastitu, jedinstvenu
+                umjetnost oslikavanja tijela. Vidimo se u jednom od Tetobitna!
+            </p>
+        </div>
+    </body>
+    <footer>
+        Copyright © 2021. Deborah Gomerčić <a href=""><img src="slike/github.png" style="width: 20px;"/></a>
+    </footer>
+</html>
---
+++ vijesti.php
@@ -0,0 +1,66 @@
+<html lang="hr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Prva zadaća</title>
+        <link rel="stylesheet" href="stil.css">
+    </head>
+    <body>
+        <header>
+            <img src="slike/banner.jpg" />
+            <nav>
+                <ul>
+                  <li><a href="index.php">Početna stranica</a></li>
+                  <li><a href="vijesti.php">Vijesti</a></li>
+                  <li><a href="#">Kontakt</a></li>
+                  <li><a href="#">O nama</a></li>
+                  <li><a href="#">Galerija</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="sadrzaj">
+            <h1>Vijesti</h1>
+            <div>
+                <img src="slike/stilovi.jpg" style="height: 100px; float: left; padding-right: 20px;"/>
+                <h2>Kako odabrati stil tetovaže?</h2>
+                Ispisivanje nekog važnog imena ili datuma na vašu kožu vjerojatno će ostati dirljiv podsjetnik, međutim,<br>ako želite biti
+                kreativni postoje brojni čimbenici o kojima morate dobro razmisliti. Oni koji već imaju nešto<br>na umu, nešto što ste možda
+                vidjeli na Pinterestu <a href="#">...Više</a><br><br>
+            </div>
+            <hr>
+            <div>
+                <img src="slike/cijena.jpg" style="height: 100px; float: left; padding-right: 20px;"/>
+                <h2>Zašto tetovaže toliko koštaju?</h2>
+                Unutar cijene koju nam tattoo umjetnik naplaćuje zbog izrade tetovaže, uključeni su mnogi troškovi koji padaju<br> izravno na
+                studij kako bi mogli funkcionirati i, logično, tetovirati. S jedne strane imamo sav materijal za jednokratnu<br>upotrebu koji
+                se koristi samo jednom <a href="#">...Više</a><br><br>
+            </div>
+            <hr>
+            <div>
+                <img src="slike/bol.jpg" style="height: 100px; float: left; padding-right: 20px;"/>
+                <h2>Razina boli tetoviranja</h2>
+                Najmanje bolnija mjesta za tetoviranje su ona s najviše masnih, najmanje živčanih završetaka i najdebljom kožom.<br>Najviše bolna
+                mjesta za tetoviranje su ona s najmanje masnoće, najviše živčanih završetaka i tanka koža. Koštana područja<br>obično mnogo bole.
+                Svatko doživljava bol <a href="#">...Više</a><br><br>
+            </div>
+            <hr>
+            <div>
+                <img src="slike/njega.jpg" style="height: 100px; float: left; padding-right: 20px;"/>
+                <h2>Njega nakon tetoviranja</h2>
+                Vaša tetovaža je individualna kao i vaša koža, prema tome nemojte slušati savjete prijatelja i poznanika po pitanju<br>održavanja
+                jer ono što je bilo dobro za njih ne mora biti dobro i za vas, tj. za vašu tetovažu. Koža je živući organizam a<br>tetovaža je 
+                površinska rana. Ponekad <a href="#">...Više</a><br><br>
+            <hr>
+            <div>
+                <img src="slike/kako.jpg" style="height: 100px; float: left; padding-right: 20px;"/>
+                <h2>Kako tetovaža ostaje na koži?</h2>
+                Dok jedni tetovaže smatraju umjetnošću, drugi ih koriste kao način da iskažu svoj stav o nečemu, a treći ih stavljaju jer<br>su u
+                trendu. No, dosta ljudi ne zna kako zapravo nastaju tetovaže. Naime, struja uz pomoć elektromagnetizma pokreće pumpu u<br>mašinici
+                za tetoviranje koja potom <a href="#">...Više</a><br><br>
+            </div>
+        </div>
+    </body>
+    <footer>
+        Copyright © 2021. Deborah Gomerčić <a href=""><img src="slike/github.png" style="width: 20px;"/></a>
+    </footer>
+</html>
